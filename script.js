74827152bca33e83b35ed117c3e85f718d01d779
Polina <polinanizhynska@gmail.com>
2020-07-16 18:11:22 +0300

    index.html mode

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-function myFirstApp (name, age) {
 
-    alert(`Hello, my name is ${name} and this is my program`);
-   
-    function showSkills () {
-        let skills = ["html", "css", "js"];
-        for(let i = 0; i > skills.length; i++) {
-           return alert("i know: " + skills[i]);
-        }
-    }
-        function checkAge() {
-            if(age == 18) {
-                alert("great chences");
-            } else if( age < 18) {
-                alert("cool you are think about future");
-            }
-    }
-
+let options = {
+    width: 1024,
+    height: 1024,
+    name: "test",
 }
-myFirstApp ("ivan", 14);
+console.log(options.name);
+options.bool = false;
+options.colors = {
+    border: "black",
+    background: "red"
+};
+delete options.bool;
+console.log(options);
+for(let key in options) {
+    console.log("свойство" + key + " имеет значение" + options[key]);
+};
